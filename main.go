25faf664a4def3b459a92ea4eab0e10de604d55e
Tesfahun Kebede <tesfahunkebede336@gmail.com>
2023-12-30 01:38:46 +0300

    setup route config

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"simple_api/src/app/routes"
 	"simple_api/src/config"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.InitialSetups()
+
+	router := gin.Default()
+
+	routes.RegisterAllRoutes(router)
+
+	err := router.Run("localhost:8080")
+
+	if err != nil {
+
+		panic("[Erro] failed to start server" + err.Error())
+
+	}
 
 }
--- src/app/routes/routes.go
+++ src/app/routes/routes.go
@@ -1,5 +1,17 @@
 package routes
 
-func RegisterAllRoutes() {
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterAllRoutes(api *gin.Engine) {
+
+	router := api.Group("app")
+	// setup all routes here
+	router.GET("/hello", func(context *gin.Context) {
+		context.String(http.StatusOK, "First Man Saying Ello Ello al internet")
+	})
 
 }
